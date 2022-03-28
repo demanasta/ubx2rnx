347b2107c3fa865a1e2e69aec5fd804c3fcf91d3
mitsos.proion <dganastasiou@gmail.com>
2022-03-28 16:10:18 +0300

    last upd dev

---
+++ run_anubis.sh
@@ -0,0 +1,8 @@
+#! /bin/env bash
+anubis :gen:beg "${1} 00:00:00" \
+                :gen:end "${1} 23:59:59" \
+                :gen:int 30 \
+                :inp:rinexo ${2} \
+                :inp:rinexn ${3} \
+                :out:xtr ${4} \
+                -x ${5}
